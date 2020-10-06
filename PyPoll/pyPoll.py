20dbadd2055ac610b61cb8effc61901c44338977
Siliconboy2 <colin.daniels@nandor.net>
2020-10-06 17:45:52 -0500

    Update pyPoll.py

--- PyPoll/pyPoll.py
+++ PyPoll/pyPoll.py
@@ -9,12 +9,10 @@ names = [{'name': 'Khan', 'votes': 0, 'percent':  0},
          {'name': 'Li', 'votes': 0, 'percent': 0},
          {'name': "O'Tooley", 'votes': 0, 'percent': 0}]
 
-
 def total(candidate):
     if row['Candidate'] == candidate['name']:
         candidate['votes'] += 1
     candidate['percent'] = round((candidate['votes'] / vote_count) * 100, 2)
-
 
 with open('/Users/northwestern/PycharmProjects/homework/PyPoll/election_data.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -23,14 +21,12 @@ with open('/Users/northwestern/PycharmProjects/homework/PyPoll/election_data.csv
         unfiltered_candidates.append(row['Candidate'])
         for i in names:
             total(i)
-
     for i in unfiltered_candidates:
         if i not in candidate_list:
             candidate_list.append(i)
-
 print('Election Results\n-------------------------')
 print(f'Total Votes: {vote_count}\n-------------------------')
 for i in names:
     print(f"{i['name']}: {i['percent']}% ({i['votes']})")
-
-
+winnerTuple = max([(d['votes'], d['name']) for d in names])
+print(f'-------------------------\nWinner: {winnerTuple[1]}\n-------------------------')
