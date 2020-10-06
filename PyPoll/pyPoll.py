2f0a76b83f9e91271ed723bcc6394afb69f0fca4
Siliconboy2 <colin.daniels@nandor.net>
2020-10-06 17:16:10 -0500

    Add files via upload

---
+++ PyPoll/pyPoll.py
@@ -0,0 +1,36 @@
+import csv
+
+vote_count = 0
+unfiltered_candidates = []
+candidate_list = []
+
+names = [{'name': 'Khan', 'votes': 0, 'percent':  0},
+         {'name': 'Correy', 'votes': 0, 'percent': 0},
+         {'name': 'Li', 'votes': 0, 'percent': 0},
+         {'name': "O'Tooley", 'votes': 0, 'percent': 0}]
+
+
+def total(candidate):
+    if row['Candidate'] == candidate['name']:
+        candidate['votes'] += 1
+    candidate['percent'] = round((candidate['votes'] / vote_count) * 100, 2)
+
+
+with open('/Users/northwestern/PycharmProjects/homework/PyPoll/election_data.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        vote_count += 1
+        unfiltered_candidates.append(row['Candidate'])
+        for i in names:
+            total(i)
+
+    for i in unfiltered_candidates:
+        if i not in candidate_list:
+            candidate_list.append(i)
+
+print('Election Results\n-------------------------')
+print(f'Total Votes: {vote_count}\n-------------------------')
+for i in names:
+    print(f"{i['name']}: {i['percent']}% ({i['votes']})")
+
+
